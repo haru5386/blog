98351757a9b3b90532488f7e28adec5ce0ba4908
Chloe <chloe.l@nexusplay.tw>
2025-04-21 17:59:31 +0800

    temp

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import { getPostBySlug, getAllPosts } from '@/lib/blog-utils';
+import Link from "next/link";
+import { getPostBySlug, getAllPosts } from "@/lib/blog-utils";
 
-import { CustomMDX } from '@/components/blog/mdx'
+import { CustomMDX } from "@/components/blog/mdx";
 type Params = Promise<{
   category: string;
   slug: string;
-}>
+}>;
 
 export async function generateStaticParams() {
   const posts = await getAllPosts();
@@ -15,10 +15,8 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function PostPag(
-  { params }: { params: Params }
-){
-  const { category, slug } = await params
+export default async function PostPag({ params }: { params: Params }) {
+  const { category, slug } = await params;
   const post = await getPostBySlug(category, slug);
 
   if (!post) {
@@ -38,24 +36,23 @@ export default async function PostPag(
   return (
     <main className="flex-1 md:max-w-4xl max-w-[calc(100vw-48px)] mb-10">
       <div className="md:max-w-4xl mx-auto max-w-[calc(100vw-48px)]">
-
-      <article>
-        <h1>{post.title}</h1>
-        <div className="flex flex-wrap gap-4 text-sm text-muted-foreground mt-4 mb-8 border-b border-border pb-6">
-          <time dateTime={post.date}>{post.date}</time>
-          <div className="flex gap-2 flex-wrap">
-            {post.tags.map((tag) => (
-              <span
-                key={tag}
-                className="text-xs px-2 py-1 bg-muted rounded-full"
-              >
-                {tag}
-              </span>
-            ))}
+        <article>
+          <h1>{post.title}</h1>
+          <div className="flex flex-wrap gap-4 text-sm text-muted-foreground mt-4 mb-8 border-b border-border pb-6">
+            <time dateTime={post.date}>{post.date}</time>
+            <div className="flex gap-2 flex-wrap">
+              {post.tags.map((tag) => (
+                <span
+                  key={tag}
+                  className="text-xs px-2 py-1 bg-muted rounded-full"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
           </div>
-        </div>
-        <CustomMDX source={post.content} />
-      </article>
+          <CustomMDX source={post.content} />
+        </article>
       </div>
     </main>
   );
